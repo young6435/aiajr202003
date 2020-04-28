7ae45a6b6fe63ef0a864dc06b8dc86e324a148be
BIT02-2300 <00>
2020-04-28 17:52:06 +0900

    예외처리

--- java_project/Cp12-ExceptionHandling/src/ex/DivideByZero.java
+++ java_project/Cp12-ExceptionHandling/src/ex/DivideByZero.java
@@ -3,26 +3,31 @@ package ex;
 public class DivideByZero {
 
 	public static void main(String[] args) {
-		
+
 		int num1 = 10;
 		int num2 = 0;
-			
+
 		try {
-			
-			// 예외 발생 지점.
+
+			// 예외 발생 지점
 			int divide = num1 / num2;
-			System.out.println("연산의 결과 : " + divide);
-		
-		}catch(ArithmeticException e) {
-			
+			System.out.println("연산의 결과는 : " + divide);
+
+		} catch (ArithmeticException e) {
 			System.out.println("0으로 나눌수 없습니다.");
 			System.out.println(e.getMessage());
-			e.printStackTrace();		// 에러 난거 확인할때 쓴다. 몇번째 줄에서 에러났는지 나온다.
+			e.printStackTrace();
+			
+			return;
+
+		} finally {
+			System.out.println("무조건 실행하는 finally 블록");
 		}
 		
-		System.out.println("프로그램을 종료합니다.");
+		System.out.println("프로그램을 종료 합니다.");
+
 		
-	
+
 	}
 
 }
