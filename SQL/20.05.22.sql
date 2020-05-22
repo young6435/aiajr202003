fffd90bb4d0c26f8032c6cf9d435ef124942d498
BIT02-2300 <00>
2020-05-22 12:44:38 +0900

    수업

--- SQL/20.05.22.sql
+++ SQL/20.05.22.sql
@@ -1,7 +1,7 @@
 -- 2020.05.22. 금요일
 
 
--- sub query(서브 쿼리)
+-- sub query(서브 쿼리)     -- 조인, 서브쿼리 같은 느낌인데, 서브쿼리 막쓰면 성능 떨어진다.
 
 
 -- 평균 급여를 구하는 쿼리문을 서브 쿼리로 사용하여 
@@ -134,6 +134,115 @@ from orders o
 group by custid
 ;
 
+---------------------------------------------------
 
+-- 인라인 뷰
+
+-- 질의 고객번호가 2 이하인 고객의 판매액을 보이시오
+-- (결과는 고객이름과 고객별 판매액 출력)
+
+select * from customer where custid <= 2;
+
+select cs.name
+from customer cs, orders od
+where cs.custid = od.custid
+group by cs.name
+;
+
+
+select cs.name,  sum(saleprice)
+from (select * from customer where custid <= 2) cs, orders od       -- customer 자리에 들어간다.
+where cs.custid = od.custid
+group by cs.name
+;
+
+
+-- rownum을 이용하기.
+
+select rownum, ename from emp;
+
+
+select rownum, ename, empno, sal
+from (select ename, empno, job, deptno, sal from emp order by sal desc)
+where rownum <= 3       -- 상위 3명만 보겠다.
+;
+
+
+-- 평균 주문금액 이하의 주문에 대해서 
+-- 주문번호와 금액을 보이시오.
+
+-- 평균주문금액 쿼리
+select avg(saleprice) from orders;      -- 11800
+
+select orderid, saleprice 
+from orders
+where saleprice <= (select avg(saleprice) from orders)
+;
+
+
+-- 각 고객의 평균 주문금액보다 큰 금액의 주문 내역에 대해서 
+-- 주문번호, 고객번호, 금액을 보이시오.
+
+-- 각 고객의 평균주문금액
+select avg(saleprice) from orders where custid=1;           -- 13000
+
+select orderid, custid, o.saleprice
+from orders o
+where saleprice > (select avg(saleprice) from orders a where a.custid = o.custid)
+;
+
+
+-- 대한민국에 거주하는 고객에게 판매한 도서의 총판매액을 구하시오.
+
+-- 대한민국에 거주하는 고객
+select custid from customer where address like '%대한민국%';
+
+select sum(saleprice)
+from orders
+where custid in (select custid from customer where address like '%대한민국%')
+;
+
+
+-- 3번 고객이 주문한 도서의 최고 금액보다 
+-- 더 비싼 도서를 구입한 주문의 주문번호와 금액을 보이시오.
+
+-- 3번 고객이 주문한 도서 금액
+select * from orders where custid=3 order by saleprice desc;    
+select max(saleprice) from orders where custid=3;      -- 13000
+
+select orderid, saleprice
+from orders
+where saleprice > all (select saleprice from orders where custid=3)
+;
+
+
+-- EXISTS 연산자로 대한민국에 거주하는 고객에게 
+-- 판매한 도서의 총 판매액을 구하시오.
+
+-- 대한민국에 거주하는 고객
+select * from customer where address like '%대한민국%';
+
+select sum(saleprice)       -- 1번 방법.
+from orders o
+where EXISTS (
+
+select * 
+from customer c
+where address like '%대한민국%' and o.custid = c.custid
+
+)
+;
+
+
+select sum(saleprice)       -- 2번 방법.
+from orders o
+where o.custid in (
+
+select c.custid 
+from customer c
+where address like '%대한민국%' and o.custid = c.custid
+
+)
+;
 
 
